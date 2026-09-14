836fe0ae2162282b38487dc33c97998f28410d42
Juan Mejia <reallifejuan2@gmail.com>
2026-09-14 09:57:51 -0400

    Found Centipawn Evaluation

--- scratchpad.sql
+++ scratchpad.sql
@@ -1,11 +1,13 @@
-INSTALL aixchess FROM community;
-LOAD aixchess;
+INSTALL aixchess FROM community; LOAD aixchess;
 
 SELECT 
     white, 
     black, 
     result, 
-    white_rating,
+    evals[1] AS centipawns,
     move_details_at(movedata, 0) AS first_move
 FROM games_sample
+WHERE evals IS NOT NULL
 LIMIT 10;
+
+DESCRIBE games_sample;
